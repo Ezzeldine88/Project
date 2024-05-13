df4dab30f788f5c654241abd4fb0a19e0a239961
youssefomarr <142510639+youssefomarr@users.noreply.github.com>
2024-05-13 22:11:45 +0300

    Update cashDonation.php

--- Donation/Controller/cashDonation.php
+++ Donation/Controller/cashDonation.php
@@ -1,1 +1,13 @@
+include 'DonationModel.php';
+$conn = new mysqli("localhost", "root", "", "clinic");  // Ideally, connection details should be in a config file or injected.
 
+$donationModel = new DonationModel($conn);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $result = $donationModel->addCashDonation($_POST['name'], $_POST['email'], $_POST['phone'], $_POST['amount'], $_POST['payment'], $_POST['message']);
+    if ($result) {
+        echo "Thank you for your donation!";
+    } else {
+        echo "Error: Something went wrong.";
+    }
+}
