b8e5b12883c277db8312c9d029d2cb9bd95dcabb
ahmedmuhamed02 <167019920+ahmedmuhamed02@users.noreply.github.com>
2024-05-14 16:02:05 +0300

    Update getAllAppointmentsCont.php

--- Appointmet 2/Controller/getAllAppointmentsCont.php
+++ Appointmet 2/Controller/getAllAppointmentsCont.php
@@ -5,28 +5,26 @@ class GetAllAppointmentsController {
     private $model;
 
     public function __construct($conn) {
-        // Create an instance of the model
+        
         $this->model = new GetAllAppointments($conn);
     }
 
     public function displayAllAppointments() {
-        // Call the method in the model to fetch all appointments
+        
         $appointments = $this->model->displayAllAppointment();
 
-        // Include the view file and pass appointments data
-        require 'getAllAppointmentsview.php'; // Replace 'your_html_view_file.php' with the actual file name of your HTML view
+        
+        require 'getAllAppointmentsview.php';
     }
 }
 
-// Example usage:
 
-// Create a new instance of the controller and pass the database connection
 $conn = new mysqli("localhost","root","","clinic");
 $controller = new GetAllAppointmentsController($conn);
 
-// Call the method to display all appointments
+
 $controller->displayAllAppointments();
 
-// Close the database connection
+
 $conn->close();
 ?>
